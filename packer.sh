de7438bfea8b877d2625d5ec5694c14f25fe2548
sivaram <sivaram0434@gmail.com>
2024-08-19 15:55:41 +0530

    packer chages

--- packer.sh
+++ packer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget https://releases.hashicorp.com/packer/1.9.2/packer_1.9.2_linux_amd64.zip
 unzip packer_1.9.2_linux_amd64.zip
+rm -rf packer_1.9.2_linux_amd64.zip
 sudo mv packer /usr/local/bin/
 packer --version
