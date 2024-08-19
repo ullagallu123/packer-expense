5748434a5f8733a066868830682ac569ac1ec6bd
sivaram <sivaram0434@gmail.com>
2024-08-19 15:51:53 +0530

    comment extra

--- expense/frontend/frontend.sh
+++ expense/frontend/frontend.sh
@@ -133,46 +133,46 @@ echo "Script execution completed successfully." | tee -a "$LOG_FILE"
 
 
 
-events {}
+# events {}
 
-http {
-    include /etc/nginx/mime.types;
-    default_type application/octet-stream;
+# http {
+#     include /etc/nginx/mime.types;
+#     default_type application/octet-stream;
 
-    # Log format
-    log_format main '$remote_addr - $remote_user [$time_local] "$request" '
-                    '$status $body_bytes_sent "$http_referer" '
-                    '"$http_user_agent" "$http_x_forwarded_for"';
+#     # Log format
+#     log_format main '$remote_addr - $remote_user [$time_local] "$request" '
+#                     '$status $body_bytes_sent "$http_referer" '
+#                     '"$http_user_agent" "$http_x_forwarded_for"';
 
-    access_log /var/log/nginx/access.log main;
-    error_log /var/log/nginx/error.log;
+#     access_log /var/log/nginx/access.log main;
+#     error_log /var/log/nginx/error.log;
 
-    # Default server configuration
-    server {
-        listen 80 default_server;
-        server_name localhost;
+#     # Default server configuration
+#     server {
+#         listen 80 default_server;
+#         server_name localhost;
 
-        root /usr/share/nginx/html;
-        index index.html;
+#         root /usr/share/nginx/html;
+#         index index.html;
 
-        location / {
-            try_files $uri $uri/ =404;
-        }
+#         location / {
+#             try_files $uri $uri/ =404;
+#         }
 
-        location /api/ {
-            proxy_pass http://backend.test.ullagallu.cloud:8080/;
-            proxy_http_version 1.1;
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-        }
+#         location /api/ {
+#             proxy_pass http://backend.test.ullagallu.cloud:8080/;
+#             proxy_http_version 1.1;
+#             proxy_set_header Host $host;
+#             proxy_set_header X-Real-IP $remote_addr;
+#             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#             proxy_set_header X-Forwarded-Proto $scheme;
+#         }
         
-        location /health {
-            stub_status on;
-            access_log off;
-        }
+#         location /health {
+#             stub_status on;
+#             access_log off;
+#         }
 
-        # Additional location blocks or configurations can go here
-    }
-}
+#         # Additional location blocks or configurations can go here
+#     }
+# }
