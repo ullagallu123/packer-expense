5dbc852db6e7c585ae454b09a471a7379142a652
sivaram <sivaram0434@gmail.com>
2024-08-21 17:19:31 +0530

    made the changes

--- rabbit.sh
+++ rabbit.sh
@@ -7,23 +7,16 @@ HOSTED_ZONE_ID="Z0734300103KWSRCOUTDI"
 RECORD_NAME="rb-rabbit.test.ullagallu.cloud"
 TTL="1"
 
-# Define RabbitMQ broker details
-ENGINE_TYPE="RABBITMQ"
-DEPLOYMENT_MODE="SINGLE_INSTANCE"
-BROKER_INSTANCE_TYPE="mq.t3.micro"
-USER_NAME="roboshop"
-USER_PASSWORD="roboshop123"
-
 # Function to create the broker
 create_broker() {
     echo "Creating Amazon MQ broker '$BROKER_NAME'..."
 
     CREATE_OUTPUT=$(aws mq create-broker \
         --broker-name "$BROKER_NAME" \
-        --engine-type "$ENGINE_TYPE" \
-        --deployment-mode "$DEPLOYMENT_MODE" \
-        --host-instance-type "$BROKER_INSTANCE_TYPE" \
-        --users Username="$USER_NAME",Password="$USER_PASSWORD" \
+        --engine-type "RABBITMQ" \
+        --deployment-mode "SINGLE_INSTANCE" \
+        --host-instance-type "mq.t3.micro" \
+        --users Username="roboshop",Password="roboshop123" \
         --publicly-accessible \
         --region "$REGION" \
         --output json)
@@ -33,7 +26,6 @@ create_broker() {
         exit 1
     fi
 
-    # Extract broker ID from create output
     BROKER_ID=$(echo "$CREATE_OUTPUT" | jq -r '.BrokerId')
 
     if [ -z "$BROKER_ID" ]; then
@@ -53,7 +45,7 @@ get_broker_endpoint() {
         --region "$REGION" \
         --output json)
 
-    BROKER_ENDPOINT=$(echo "$BROKER_DETAILS" | jq -r '.BrokerInstances[0].Endpoints[0]' | sed -e 's/amqps:\/\///' -e 's/:5671//')
+    BROKER_ENDPOINT=$(echo "$BROKER_DETAILS" | jq -r '.BrokerInstances[0].Endpoints[0]' | sed 's/amqps:\/\///' | sed 's/:5671//')
 
     if [ -z "$BROKER_ENDPOINT" ]; then
         echo "Failed to fetch broker endpoint."
