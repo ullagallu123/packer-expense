10353cf676339d09f6cbac6a709d0a25b2a3a76b
sivaram <sivaram0434@gmail.com>
2024-08-22 23:18:44 +0530

    made the changes

--- ecs/service/mysql.sh
+++ ecs/service/mysql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 aws ecs create-service \
     --cluster roboshop-cluster \
-    --service-name cart-service \
-    --task-definition cart \
+    --service-name mysql-service \
+    --task-definition mysql \
     --desired-count 1 \
     --launch-type FARGATE \
     --network-configuration "awsvpcConfiguration={subnets=[subnet-0731498b80aa56290],securityGroups=[sg-00c0933258e06cea0],assignPublicIp=ENABLED}" \
