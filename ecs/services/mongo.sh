58c74fe9b5af7a250948e61614d746a3db13553a
sivaram <sivaram0434@gmail.com>
2024-08-22 18:32:03 +0530

    made the changes

--- ecs/services/mongo.sh
+++ ecs/services/mongo.sh
@@ -4,7 +4,6 @@ aws ecs create-service \
     --task-definition mongo \
     --desired-count 1 \
     --launch-type FARGATE \
-    --network-configuration "awsvpcConfiguration={subnets=[subnet-0731498b80aa56290],securityGroups=[sg-00c0933258e06cea0]}" \
-    --service-registries "registryArn=arn:aws:servicediscovery:ap-south-1:8069-6216-9196:service/srv-pqhbd5wmnk33g5za"
+    --network-configuration "awsvpcConfiguration={subnets=[subnet-0731498b80aa56290],securityGroups=[sg-00c0933258e06cea0],assignPublicIp=ENABLED}" 
 
 #This is my ServiceID: srv-pqhbd5wmnk33g5za
