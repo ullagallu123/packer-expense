a9fa7a53364db7b93964c983a77f7e033305f4cd
sivaram <sivaram0434@gmail.com>
2024-08-22 18:46:47 +0530

    made the changes mysql

--- ecs/task-definitions/mysql.sh
+++ ecs/task-definitions/mysql.sh
@@ -1,12 +1,14 @@
 aws ecs register-task-definition \
     --family mysql \
     --network-mode awsvpc \
+    --requires-compatibilities FARGATE \
+    --cpu "256" \
+    --memory "512" \
+    --execution-role-arn arn:aws:iam::806962169196:role/ecsTaskExecutionRole1 \
     --container-definitions '[
         {
             "name": "mysql",
             "image": "siva9666/mysql-instana:v1",
-            "memory": 512,
-            "cpu": 256,
             "essential": true,
             "environment": [
                 {
@@ -19,6 +21,14 @@ aws ecs register-task-definition \
                     "containerPort": 3306,
                     "protocol": "tcp"
                 }
-            ]
+            ],
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-group": "/ecs/mysql",
+                    "awslogs-region": "ap-south-1",
+                    "awslogs-stream-prefix": "ecs"
+                }
+            }
         }
     ]'
