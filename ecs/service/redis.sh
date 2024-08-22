b1066e426b6051d8ca27b654b50d5bc6d1529e00
sivaram <sivaram0434@gmail.com>
2024-08-22 23:25:24 +0530

    made the changes

--- ecs/service/redis.sh
+++ ecs/service/redis.sh
@@ -6,4 +6,4 @@ aws ecs create-service \
     --desired-count 1 \
     --launch-type FARGATE \
     --network-configuration "awsvpcConfiguration={subnets=[subnet-0731498b80aa56290],securityGroups=[sg-00c0933258e06cea0],assignPublicIp=ENABLED}" \
-    --service-registries "registries=[{registryArn=arn:aws:servicediscovery:ap-south-1:8069-6216-9196:service/srv-pqhbd5wmnk33g5za}]"
+    --service-registries "registryArn=arn:aws:servicediscovery:ap-south-1:8069-6216-9196:service/srv-pqhbd5wmnk33g5za"
