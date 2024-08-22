06a2fbb19cdb7ad9d7347389b864c9e9ffd63760
sivaram <sivaram0434@gmail.com>
2024-08-22 23:11:37 +0530

    made the changes

--- ecs/2.ns.sh
+++ ecs/2.ns.sh
@@ -1,16 +1,16 @@
 
 
 
-# aws servicediscovery create-private-dns-namespace \
-#     --name roboshop.local \
-#     --vpc vpc-07e30bcef51446692 \
-#     --query 'Namespace.Id' \
-#     --output text
+aws servicediscovery create-private-dns-namespace \
+    --name roboshop.local \
+    --vpc vpc-07e30bcef51446692 \
+    --query 'Namespace.Id' \
+    --output text
 
-# aws servicediscovery create-service \
-#     --name roboshop \
-#     --namespace-id ns-vejejj7lhxn6m66d \
-#     --dns-config "NamespaceId=ns-vejejj7lhxn6m66d,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=1}]" \
-#     --query 'Service.Id' \
-#     --output text
+aws servicediscovery create-service \
+    --name roboshop \
+    --namespace-id ns-vejejj7lhxn6m66d \
+    --dns-config "NamespaceId=ns-vejejj7lhxn6m66d,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=1}]" \
+    --query 'Service.Id' \
+    --output text
 
