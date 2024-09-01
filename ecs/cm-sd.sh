c3fa0d263cd74992658543b43fef05cbfce164ba
sivaram <sivaram0434@gmail.com>
2024-09-01 15:59:12 +0530

    added sd for all srvs

---
+++ ecs/cm-sd.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+aws servicediscovery create-service \
+    --name mysql \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
+
+aws servicediscovery create-service \
+    --name mongo \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
+
+aws servicediscovery create-service \
+    --name redis \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
+
+aws servicediscovery create-service \
+    --name rabbit \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
+
+aws servicediscovery create-service \
+    --name catalogue \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
+
+aws servicediscovery create-service \
+    --name cart \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
+
+aws servicediscovery create-service \
+    --name user \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
+
+aws servicediscovery create-service \
+    --name shipping \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
+
+aws servicediscovery create-service \
+    --name payment \
+    --namespace-id ns-dqhk4l7ogtngs2wi \
+    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=MULTIVALUE,DnsRecords=[{Type=A,TTL=60}]"
