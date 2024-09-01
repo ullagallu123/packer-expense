1c7cb8dc23fc488ca64a144c39ebc9f66e069983
sivaram <sivaram0434@gmail.com>
2024-09-01 15:35:57 +0530

    made the changes

---
+++ ecs/sd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#!/bin/bash
+
+# List all services in the cluster
+services=$(aws ecs list-services --cluster instana --query "serviceArns[]" --output text)
+
+# Loop through each service and delete it
+for service in $services; do
+    echo "Deleting service: $service"
+    aws ecs delete-service --cluster instana --service "$service" --force
+done
