0e168ae7e631de6644545ad0393ace7de4dddbfb
sivaram <sivaram0434@gmail.com>
2024-09-01 14:32:38 +0530

    made the changes

--- ecs-1/cluster/c-map.sh
+++ ecs-1/cluster/c-map.sh
@@ -2,4 +2,5 @@
 aws servicediscovery create-private-dns-namespace \
   --name instana \
   --vpc vpc-07e30bcef51446692 \
-  --query "Namespace.Id" --output text
+  --query "Namespace.Id" \
+  --output text
