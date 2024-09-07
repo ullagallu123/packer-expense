69fb5e1b72f7b86fabbf7e997a8b382b1f947fec
sivaram <sivaram0434@gmail.com>
2024-09-07 15:10:55 +0530

    remove space from cart definition

--- ecs/service/cart.sh
+++ ecs/service/cart.sh
@@ -6,4 +6,4 @@ aws ecs create-service \
     --desired-count 1 \
     --launch-type FARGATE \
     --network-configuration "awsvpcConfiguration={subnets=[subnet-090cf41d5e0cdf437],securityGroups=[sg-0ca8167841704f04d],assignPublicIp=ENABLED}" \
-    --service-registries "registryArn=arn:aws:servicediscovery:ap-south-1:4273-6630-1535:service/ srv-pd3oigwz37gqv3x7"
+    --service-registries "registryArn=arn:aws:servicediscovery:ap-south-1:4273-6630-1535:service/srv-pd3oigwz37gqv3x7"
