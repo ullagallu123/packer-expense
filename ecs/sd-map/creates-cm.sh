8a2d0100b82660dac27936b4dc1632fbe439cfc2
sivaram <sivaram0434@gmail.com>
2024-09-07 12:51:36 +0530

    sd cm

--- ecs/sd-map/creates-cm.sh
+++ ecs/sd-map/creates-cm.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 aws servicediscovery create-service \
     --name mongo \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name redis \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name rabbit \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name catalogue \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name cart \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name user \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name shipping \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name payment \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name web \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery create-service \
     --name debug \
-    --namespace-id ns-dqhk4l7ogtngs2wi \
-    --dns-config "NamespaceId=ns-dqhk4l7ogtngs2wi,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
+    --namespace-id ns-4ek4go5zbr2xphlu \
+    --dns-config "NamespaceId=ns-4ek4go5zbr2xphlu,RoutingPolicy=WEIGHTED,DnsRecords=[{Type=A,TTL=60}]"
 
 aws servicediscovery list-services \
     --query "Services[*].[Id,Name]" \
