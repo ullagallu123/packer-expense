88ed215e115c53c0aa7ea629d1cfb101689170f4
sivaram <sivaram0434@gmail.com>
2024-09-08 15:48:37 +0530

    adding log dash boards

---
+++ ecs/3.cw/log-dashboard-delete.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the region
+region="ap-south-1"
+
+# List all dashboards and extract their names
+dashboard_names=$(aws cloudwatch list-dashboards --region ${region} --query "DashboardEntries[*].DashboardName" --output text)
+
+# Loop through each dashboard name and delete it
+for dashboard_name in ${dashboard_names}; do
+  if [[ $dashboard_name == *"-log-dashboard" ]]; then
+    echo "Deleting dashboard: ${dashboard_name}"
+    aws cloudwatch delete-dashboard --dashboard-name "${dashboard_name}" --region ${region}
+  fi
+done
